14c95b6e0803549338b3654dd5846278c6827641
Aigatr <thomas@aigatr.com>
2022-12-23 07:54:04 +0100

    Base commit
    Added functionality for the buttons

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public Button StartButton;
+    public TMP_InputField NameInputField;
+    public Button ExitButton;
+
+    private Text NameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NameInputField.Select();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(NameInputField.text == "")
+        {
+            StartButton.interactable= false;
+        }
+        else
+        {
+            StartButton.interactable = true;
+        }
+
+    }
+
+    public void StartNewScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+
+    }
+}
